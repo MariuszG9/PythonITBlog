82366afe90671e31b5e01101ffe43a34a63e2dfc
MariuszG9 <120599403+MariuszG9@users.noreply.github.com>
2022-12-28 17:28:14 +0100

    Create 02-series-and-data-frame-hand.py

---
+++ 02-series-and-data-frame-hand.py
@@ -0,0 +1,21 @@
+# zaimportowanie biblioteki
+import pandas as pd
+
+# tworzymy słownik list
+slownik_zwierzat = {'Gatunek': ['Ssaki',
+                                'Ptaki',
+                                'Płazy',
+                                'Gady',
+                                'Ryby'
+                              ],
+                  'Zwierze': ['Tygrys',
+                              'Tukan',
+                              'Żaba',
+                              'Jaszczurka',
+                              'Rekin'
+                             ]
+                 }
+
+# tworzymy ramkę danych
+zwierzeta = pd.DataFrame(slownik_zwierzat)
+print(zwierzeta)

71e863249afd5edb62e0bae95c282daa55bf4a32
MariuszG9 <120599403+MariuszG9@users.noreply.github.com>
2023-02-22 10:39:13 +0100

    Create 03-series-and-data_frame_columns2.py

---
+++ 03/03-series-and-data_frame_columns2.py
@@ -0,0 +1,20 @@
+# zaimportowanie biblioteki
+import pandas as pd
+
+# wyświetlenie większej ilości kolumn
+desired_width = 350
+pd.set_option('display.width', desired_width)
+pd.set_option('display.max_columns', 15)
+
+# wczytanie danych / należy zwrócić uwagę na sep i decimal xxx
+df_first = pd.read_csv("Dane_01.csv", sep=";", decimal=',')
+df_second = pd.read_csv("Dane_02.csv", sep=";")
+
+# łączenie danych metodą merge xxx
+df_animals = pd.merge(df_first, df_second, how='outer')
+
+# sortowanie danych po nowej kolumnie max
+df_animals['max_rank'] = df_animals['Rating'].rank(method='max', ascending=False)
+print(df_animals.max_rank)
+print(df_animals.sort_values(by='max_rank'))
+print("\n")

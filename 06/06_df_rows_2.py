2e5c55206fd470a772e67ec6f52110952e66e2dd
MariuszG9 <120599403+MariuszG9@users.noreply.github.com>
2023-02-28 16:20:59 +0100

    Create 06_df_rows_2.py

---
+++ 06/06_df_rows_2.py
@@ -0,0 +1,18 @@
+# zaimportowanie biblioteki
+import pandas as pd
+
+# wyświetlenie większej ilości kolumn
+desired_width = 350
+pd.set_option('display.width', desired_width)
+pd.set_option('display.max_columns', 15)
+
+# wczytanie danych / należy zwrócić uwagę na sep i decimal xxx
+df_first = pd.read_csv("Dane_01.csv", sep=";", decimal=',')
+df_second = pd.read_csv("Dane_02.csv", sep=";")
+
+# łączenie danych metodą merge xxx
+df_animals = pd.merge(df_first, df_second, how='outer')
+
+# nazwy kolumn
+print(df_animals.columns)
+print("\n")

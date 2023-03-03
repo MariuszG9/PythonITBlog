15269cedf557f51a7a254020fba374766138e48e
MariuszG9 <120599403+MariuszG9@users.noreply.github.com>
2023-03-03 17:00:44 +0100

    Create 07_df_job_2.py

---
+++ 07/07_df_job_2.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+
+
+# Funkcja do wczytywania wszystkich ramek będących w folderze
+def load_csv():
+    path = os.path.dirname(__file__)
+    extension = ".csv"
+
+    # lista nazw plików bez rozszerzenia
+    files = [file[:-len(extension)] for file in os.listdir(path) if file.endswith(extension)]
+
+    # pętla wczytująca pliki i tworząca ramki danych
+    for file in files:
+        # wczytanie pliku csv
+        csv_file = os.path.join(path, file + extension)
+        if os.path.isfile(csv_file):
+            if '.' in pd.read_csv(csv_file, nrows=1, sep=';', encoding='utf-8', decimal='.').columns.tolist():
+                decimal = '.'
+            else:
+                decimal = ','
+            df_name = "df_" + file.lower()
+            globals()[df_name] = pd.read_csv(csv_file, sep=";", encoding='utf-8', decimal=decimal)
+            print(f"Utworzono ramkę danych {df_name} z pliku {file}.csv")
+
+
+def data_frames():
+    print("Utworzone ramki danych:")
+    for var in globals():
+        if isinstance(globals()[var], pd.core.frame.DataFrame):
+            print(var)
